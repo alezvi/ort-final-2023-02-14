480759ab32d9a3d4c897fd9ef78e8b3cc2599d73
Nelson Manoche <nmanoche@oficinacys.com.ar>
2023-02-14 20:01:24 -0300

    se agregan validaciones en el endpoint initial_calls

--- routes/calls.js
+++ routes/calls.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({});
+// Recibi los datos de la llamada de telefono
+router.post('/initial_calls', function(req, res, next) {
+  let callData = req.body;
+  let { phoneNumberFrom, phoneNumberTo, timeInit } = callData
+  try{
+    if(Object.keys(callData).length === 0) {
+      throw new Error('BAD REQUEST: ALL FIELDS ARE REQUIRED')
+    }
+    if(!phoneNumberFrom) {
+      throw new Error('BAD REQUEST: PHONE NUMBER FROM IS REQUIRED')
+    }
+    if(!phoneNumberTo) {
+      throw new Error('BAD REQUEST: PHONE NUMBER TO IS REQUIRED')
+    }
+    if(!timeInit) {
+      throw new Error('BAD REQUEST: TIME INIT IS REQUIRED')
+    }
+    
+  }catch(error) {
+    res.status(400).json({message: error.message});
+  }
+  
 });
 
 module.exports = router;
