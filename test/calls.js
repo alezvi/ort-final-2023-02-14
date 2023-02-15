3829c537e6f3924889176464de4a238032ca9911
Nelson Manoche <nmanoche@oficinacys.com.ar>
2023-02-14 21:54:23 -0300

    agregado otro test

--- test/calls.js
+++ test/calls.js
@@ -19,5 +19,18 @@ describe('Registro de llamadas', function() {
                 assert.equal(res.body.message, 'ALL FIELDS ARE REQUIRED')
             })
         })
+
+        it('Que el endpoint responda con 201 al crear la llamada', function() {
+            return request(app)
+            .post('/calls//initial_calls')
+            .send(
+                {
+                    "phoneNumberFrom": "1112341234",
+                    "phoneNumberTo": "2212341234",
+                    "timeInit": "2023-02-14 20:16:00"
+                }
+            )
+            .expect(201)
+        })
     })
 })
