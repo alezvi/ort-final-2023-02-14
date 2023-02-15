8865fc7b7951a612fdafaf9aca41bf3256c08215
Nelson Manoche <nmanoche@oficinacys.com.ar>
2023-02-14 21:24:00 -0300

    se agregan los metodos en el repo para calcular y registrar los cargos por una llamada

--- src/repositories/callcharges.js
+++ src/repositories/callcharges.js
@@ -1,1 +1,43 @@
-const { } = require('../../db/models')
+const { CallCharge, Call } = require('../../db/models')
+const TO_60 = 1;
+const TO_180 = 0.85;
+const MORE_180 = 0.75;
+
+const setCallCharges = async (idCallRegistered) => {
+    let callRegister = await Call.findOne({
+        where: {
+            id: idCallRegistered
+        }
+    });
+    let dateFinish = callRegister.timeFinish;
+    let dateInit = callRegister.timeInit;
+
+    let totalCallTime = calculateTotalTime(dateInit, dateFinish);
+    let totalToPay = calculateTotalToPay(totalCallTime);
+
+    return await CallCharge.create({
+        totalCallTime,
+        totalToPay,
+        callId: idCallRegistered
+    })
+}
+
+const calculateTotalTime = (dateInit, dateFinish) => {
+    let difference = dateFinish.getTime() - dateInit.getTime();
+    let minutes = Math.round(difference / 60000);
+    return minutes;
+};
+
+const calculateTotalToPay = (totalCallTime) => {
+    if(totalCallTime <= 60) {
+        return totalCallTime * TO_60
+    }else if(totalCallTime > 60 && totalCallTime <= 180) {
+        return totalCallTime * TO_180
+    }else {
+        return totalCallTime * MORE_180
+    }
+}
+
+module.exports = {
+    setCallCharges
+}
