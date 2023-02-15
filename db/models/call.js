070c948d5c0a6b91acfba34ec40d6e5cc22b98ac
Nelson Manoche <nmanoche@oficinacys.com.ar>
2023-02-14 21:04:02 -0300

    se agrega la relacion con la entidad  cargos por llamada

--- db/models/call.js
+++ db/models/call.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // una llamada tiene un registro de cargaos por llamar
+      Call.hasOne(models.CallCharge)
     }
   }
   Call.init({
